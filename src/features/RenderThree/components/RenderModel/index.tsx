c0008ea4e16fa5547ff247c457621c57def607ed
dannitran99 <dannitran99@gmail.com>
2026-08-25 15:39:52 +0700

    feat: 🎸 model movement path

--- src/features/RenderThree/components/RenderModel/index.tsx
+++ src/features/RenderThree/components/RenderModel/index.tsx
@@ -1,3 +1,4 @@
+import { Line } from '@react-three/drei';
 import { useLoader } from '@react-three/fiber';
 import { useRef } from 'react';
 import type { Group } from 'three';
@@ -6,6 +7,8 @@ import { type GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { useCenterModel } from '../../hooks/useCenterModel';
 import { useModelAnimation } from '../../hooks/useModelAnimation';
 import { useNodeAnimation } from '../../hooks/useNodeAnimation';
+
+import { NodeDirectionIndicator } from './NodeDirectionIndicator.tsx';
 
 type TRenderModelProps = Readonly<{
   src: string;
@@ -21,13 +24,13 @@ export const RenderModel: React.FC<TRenderModelProps> = ({ src }) => {
   //for default animation
   useModelAnimation({ gltf });
 
-  useNodeAnimation({ gltf, nodeName: 'Base' });
-
-  console.log('gltf', gltf);
+  const pathPoints = useNodeAnimation({ gltf, nodeName: 'Base', pathParentRef: groupRef });
 
   return (
     <group ref={groupRef}>
       <primitive object={gltf.scene} />
+      <NodeDirectionIndicator gltf={gltf} nodeName="Base" />
+      {pathPoints.length > 1 && <Line points={pathPoints} color="#ff6b35" lineWidth={3} />}
     </group>
   );
 };
